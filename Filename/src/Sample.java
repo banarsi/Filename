250829b7cfd92a016dabe05e80374f5151c1f5f3
banarsi <36882931+banarsi@users.noreply.github.com>
2018-05-11 12:27:52 +0530

    git2

--- Filename/src/Sample.java
+++ Filename/src/Sample.java
@@ -6,9 +6,8 @@ public class Sample
        public static void main(String[]args)
        {
     	   System.out.println("hello");
-    	   System.out.println("hello eclipse");
-  System.out.println("hello");
-                System.out.println("hello git");
+    	   System.out.println("hello git");
+ 
               
               
 
